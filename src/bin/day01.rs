1d8ecde59a4fccd1c3f8c6c29cb1ce4eac497ca7
Rodrigo V. Honorato <r.vargashonorato@uu.nl>
2024-11-30 17:18:09 +0100

    init

---
+++ src/bin/day01.rs
@@ -0,0 +1,6 @@
+fn main() {
+    // let input = read_input("input/day01.txt");
+    // let solution1 = solve1(&input);
+    // println!("Solution 1: {}", solution1);
+    println!("placeholder");
+}
